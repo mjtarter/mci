c2db4848ff436c1c3d05ee0793ff84a26c26fbd6
Matt Tarter <mjtarter@openmindwebs.com>
2015-10-19 10:37:28 -0600

    Added header/footer, configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  match ':controller(/:action(/:id))', :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
