ee4e3d0bc26f5533fdba2d6101ebd104e5ee52b6
Matt Tarter <mjtarter@openmindwebs.com>
2015-10-28 20:41:05 -0600

    Added dorms index page

---
+++ app/controllers/dorm_controller.rb
@@ -0,0 +1,2 @@
+class DormController < ApplicationController
+end
--- app/controllers/property_controller.rb
+++ app/controllers/property_controller.rb
@@ -1,7 +1,6 @@
 class PropertyController < ApplicationController
 	def create
 		if @property = Property.create(property_params)
-			flash[:notice] = '<span class="glyphicon glyphicon-music"></span> Track added to library!'
 			redirect_to(:controller => 'application', :action => 'index')
 		else
 			render('new')
