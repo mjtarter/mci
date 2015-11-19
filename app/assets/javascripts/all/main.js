93021be1fb5a99bb3ed194678eb02dc403fc62e6
Matt Tarter <mjtarter@openmindwebs.com>
2015-11-19 15:40:47 -0700

    Refactor property search pages, Add form partials

--- app/assets/javascripts/all/main.js
+++ app/assets/javascripts/all/main.js
@@ -9,3 +9,16 @@ function showDivBlock(id){
 function hideDiv(id){
 	document.getElementById(id).style.display = "none";
 }
+
+// Toggle Search Bar for Google Map
+
+function toggle() {
+	
+	if (document.getElementById('search-bar').style.display == 'block') {
+    	document.getElementById('search-bar').style.display = "none";
+		document.getElementById('toggle-text').innerHTML = '<span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span> Refine Search';
+	} else {
+    	document.getElementById('search-bar').style.display = "block";
+		document.getElementById('toggle-text').innerHTML = '<span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span> Hide Search Bar';
+	}
+}
