8266b7c28fc515c068179cc9b24f2e26a8cb2c56
Matt Tarter <mjtarter@openmindwebs.com>
2015-12-19 12:03:20 -0700

    Added roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-    has_many :properties
+ 		:recoverable, :rememberable, :trackable, :validatable
+  enum role: [ :landlord, :student ]
+		has_many :properties
 end
