6ce40d7e3d3e5967da51d3855443a44d81c5c074
Matt Tarter <mjtarter@openmindwebs.com>
2015-12-21 17:58:52 -0700

    Added form validations and default values for property uploads - location

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,6 @@
 class Property < ActiveRecord::Base
 	geocoded_by :full_address, :latitude => :lat, :longitude => :lng
-	after_validation :geocode
+	before_validation :geocode
 
 	def full_address
   		[address, city, state].compact.join(', ')
@@ -14,14 +14,14 @@ class Property < ActiveRecord::Base
 	validates_presence_of :address
 	validates_presence_of :city
 	validates :city, format: { with: /\A[a-zA-Z]+\z/, message: "can only consist of letters" }
-   	validates :city, length: { in: 2..30 }, :if => :city_format_passes
+   	validates :city, length: { in: 2..30 }
 	validates_presence_of :state
 	validates_presence_of :property_type
 	validates_acceptance_of :agreement
 	validates_presence_of :lat
-
-	def city_format_passes
-		@property.errors.invalid?(:city) == true
+  
+	before_save do |property|
+		property.city = property.city.downcase.titleize
 	end
 
 end
